4ad7872d5710c8145c0b26551f902888ca0fd1b8
Divelix <divelix1@gmail.com>
2021-02-28 13:50:07 +0300

    Quantity processing added

--- core/src/com/divelix/skitter/ui/menu/scroll/ModPage.kt
+++ core/src/com/divelix/skitter/ui/menu/scroll/ModPage.kt
@@ -1,8 +1,10 @@
 package com.divelix.skitter.ui.menu.scroll
 
 import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.scenes.scene2d.Actor
 import com.badlogic.gdx.scenes.scene2d.ui.Container
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable
+import com.badlogic.gdx.utils.SnapshotArray
 import com.divelix.skitter.data.*
 import com.divelix.skitter.scaledLabel
 import com.divelix.skitter.ui.menu.ModView
@@ -11,6 +13,7 @@ import com.divelix.skitter.ui.menu.mod.ShowcaseTable
 import com.divelix.skitter.ui.menu.tabs.Tab
 import com.divelix.skitter.ui.menu.tabs.TabbedMenu
 import ktx.collections.gdxArrayOf
+import ktx.collections.removeAll
 import ktx.inject.Context
 import ktx.scene2d.*
 import ktx.style.get
@@ -23,8 +26,8 @@ class ModPage(context: Context, val playerData: PlayerData) : Page(context), Mod
         }
     private val showcaseTable by lazy { ShowcaseTable(::sellMod, ::upgradeMod) }
     private val tabbedMenu = TabbedMenu(gdxArrayOf(
-            Tab(assets.manager.get(Constants.SHIP_ICON), StockTable(ModType.SHIP_MOD, playerData.mods, ::selectMod)),
-            Tab(assets.manager.get(Constants.GUN_ICON), StockTable(ModType.GUN_MOD, playerData.mods, ::selectMod))
+            Tab(assets.manager.get(Constants.SHIP_ICON), StockTable(ModType.SHIP_MOD, playerData.mods, ::selectMod).apply { padTop(Constants.UI_MARGIN) }),
+            Tab(assets.manager.get(Constants.GUN_ICON), StockTable(ModType.GUN_MOD, playerData.mods, ::selectMod).apply { padTop(Constants.UI_MARGIN) })
     ))
 
     init {
@@ -65,31 +68,19 @@ class ModPage(context: Context, val playerData: PlayerData) : Page(context), Mod
         if (selectedModView == null) return
         val modView = selectedModView as ModView
         val modAlias = modView.modAlias
-        modAlias.level++
-        updateStockTableFor(modAlias.type)
-        val newModView = when (modAlias.type) { //TODO find elegant solution to this
-            ModType.SHIP_MOD -> {
-                (tabbedMenu.tabs[0].contentTable as StockTable).tableWithMods.children
-                        .filter { (it as Container<*>).actor is ModView }
-                        .map { (it as Container<*>).actor as ModView }
-                        .single {
-                            it.modAlias.type == modAlias.type &&
-                                    it.modAlias.index == modAlias.index &&
-                                    it.modAlias.level == modAlias.level
-                        }
-            }
-            ModType.GUN_MOD -> {
-                (tabbedMenu.tabs[1].contentTable as StockTable).tableWithMods.children
-                        .filter { (it as Container<*>).actor is ModView }
-                        .map { (it as Container<*>).actor as ModView }
-                        .single {
-                            it.modAlias.type == modAlias.type &&
-                                    it.modAlias.index == modAlias.index &&
-                                    it.modAlias.level == modAlias.level
-                        }
-            }
+        if (modAlias.quantity == 1) {
+            modAlias.level++
+            val resultModAlias = mergeModsFor(modAlias)
+            updateStockTableFor(modAlias.type)
+            selectForModAlias(resultModAlias)
+        } else {
+            modAlias.quantity--
+            val newModAlias = ModAlias(modAlias.type, modAlias.index, modAlias.level + 1, 1)
+            playerData.mods.add(newModAlias)
+            val resultModAlias = mergeModsFor(newModAlias)
+            updateStockTableFor(modAlias.type)
+            selectForModAlias(resultModAlias)
         }
-        selectMod(newModView)
     }
 
     private fun updateStockTableFor(modType: ModType) {
@@ -97,5 +88,38 @@ class ModPage(context: Context, val playerData: PlayerData) : Page(context), Mod
             ModType.SHIP_MOD -> (tabbedMenu.tabs[0].contentTable as StockTable).reloadMods()
             ModType.GUN_MOD -> (tabbedMenu.tabs[1].contentTable as StockTable).reloadMods()
         }
+    }
+
+    // find and select upgraded mod among new ModViews
+    private fun selectForModAlias(modAlias: ModAlias) {
+        val newModView = when (modAlias.type) {
+            ModType.SHIP_MOD -> findModView(modAlias, (tabbedMenu.tabs[0].contentTable as StockTable).tableWithMods.children)
+            ModType.GUN_MOD -> findModView(modAlias, (tabbedMenu.tabs[1].contentTable as StockTable).tableWithMods.children)
+        }
+        selectMod(newModView)
+    }
+
+    // find ModView in array of actors via given ModAlias
+    private fun findModView(modAlias: ModAlias, actors: SnapshotArray<Actor>) = actors
+            .filter { (it as Container<*>).actor is ModView }
+            .map { (it as Container<*>).actor as ModView }
+            .single {
+                it.modAlias.type == modAlias.type &&
+                        it.modAlias.index == modAlias.index &&
+                        it.modAlias.level == modAlias.level &&
+                        it.modAlias.quantity == modAlias.quantity
+            }
+
+    private fun mergeModsFor(modAlias: ModAlias): ModAlias {
+        val mergeCandidates = playerData.mods
+                .filter { it.type == modAlias.type && it.index == modAlias.index && it.level == modAlias.level }
+        if (mergeCandidates.size == 1) return modAlias
+        val overallQuantity = mergeCandidates
+                .map { it.quantity }
+                .reduce { overallQuantity, quantity -> overallQuantity + quantity }
+        playerData.mods.removeAll(mergeCandidates)
+        val resultModAlias = ModAlias(modAlias.type, modAlias.index, modAlias.level, overallQuantity)
+        playerData.mods.add(resultModAlias)
+        return resultModAlias
     }
 }
