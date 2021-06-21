6de3a2328cf6eb6700d022c97a683da5558b8456
divelix <divelix1@gmail.com>
2021-06-21 13:37:55 +0300

    Upgraded to lwjgl3, upgraded kotlin and gradle versions, fixed TexturePacker

--- desktop/src/com/divelix/skitter/desktop/DesktopLauncher.kt
+++ desktop/src/com/divelix/skitter/desktop/DesktopLauncher.kt
@@ -1,23 +1,22 @@
 package com.divelix.skitter.desktop
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
 import com.divelix.skitter.data.Constants
 import com.divelix.skitter.Main
 
 object DesktopLauncher {
     @JvmStatic
     fun main(arg: Array<String>) {
-        LwjglApplication(Main(), LwjglApplicationConfiguration().apply {
-            title = Constants.TITLE
-            x = 1180
-            y = 0
-//            width = TestAIScreen.D_WIDTH // for TestAIScreen
-//            height = TestAIScreen.D_HEIGHT
-            width = Constants.DESKTOP_WIDTH
-            height = Constants.DESKTOP_HEIGHT
-//            resizable = false
-            forceExit = false // for LWJGL3
+        Lwjgl3Application(Main(), Lwjgl3ApplicationConfiguration().apply {
+            setTitle(Constants.TITLE)
+            setWindowPosition(1180, 500)
+            setWindowSizeLimits(
+                Constants.DESKTOP_WIDTH,
+                Constants.DESKTOP_HEIGHT,
+                Constants.DESKTOP_WIDTH,
+                Constants.DESKTOP_HEIGHT
+            )
         })
     }
 }
